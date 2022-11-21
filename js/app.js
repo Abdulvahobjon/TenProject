b93ef78940ce56fa46c0b609f6ace12fb0bca492
Abdulvahobjon <abdulvahob0714@gmail.com>
2022-11-21 20:15:14 +0500

    preject end

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let form  = document.querySelector('form')
+let madal  = document.querySelector('.madal')
+
+let msg = {
+    loading: "Loading... :|",
+    success: 'Thank :)',
+    error:"ERROR :("
+
+}
+
+form.addEventListener('submit' , function(e){
+    e.preventDefault()
+    const elDiv  = document.createElement('div')
+    elDiv.classList.add("modalMs")
+    madal.style.display = 'none'
+    elDiv.textContent = msg.loading
+    form.append(elDiv)
+    // 1.
+    let request  = new XMLHttpRequest()
+    // 2.
+    request.open("POST" , "./php/server.php")
+    // 3. forma elemnetlarini olish 
+    const formDate = new FormData(form)
+
+    //  
+    request.send(formDate)
+
+    request.addEventListener('load' , function(){
+        if(request.status == 200){
+            console.log(request.response);
+           setInterval(() => {
+            elDiv.textContent = msg.success
+           }, 2000);
+        }
+    })
+
+})
